aa486362103a18bb52a8db6aa1e3efc9f801602b
DJ Soults <dj@illuminateddev.com>
2018-04-07 19:23:42 -0500

    Add FizzBuzz exercise

---
+++ src/FizzBuzz/FizzLib/FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzLib
+{
+    public class FizzBuzz
+    {
+        public IDictionary<int, string> GetFizzBuzz(int min = 1, int max = 100)
+        {
+            var dictionary = new Dictionary<int, string>();
+
+            for (var i = min; i <= max; i++)
+            {
+                var value = "";
+                value = value + GetFizz(i);
+                value = value + GetBuzz(i);
+                value = PrintNumberIfEmpty(i, value);
+
+                dictionary.Add(i, value);
+            }
+
+            return dictionary;
+        }
+
+        private string PrintNumberIfEmpty(int i, string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return i.ToString();
+            }
+
+            return value;
+        }
+
+        private static string GetFizz(int i)
+        {
+            return i % 3 == 0 ? "fizz" : "";
+        }
+
+        private static string GetBuzz(int i)
+        {
+            return i % 5 == 0 ? "buzz" : "";
+        }
+
+
+    }
+}
