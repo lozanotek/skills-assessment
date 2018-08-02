8453452515864d1c3ce307a0626f88bfd87499f9
Rusty Davis <rustydavis2@gmail.com>
2018-08-02 00:31:00 -0500

    FizzBuzz check-in

---
+++ src/FizzClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using FizzLib;
+using System;
+using System.Collections.Generic;
+
+namespace FizzClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            IEnumerable<string> list = fizzBuzz.GetFizzBuzz();
+            foreach(string val in list)
+            {
+                Console.WriteLine(val);
+            }
+
+            IEnumerable<string> list2 = fizzBuzz.GetFizzBuzz(1, Int32.MaxValue);
+            foreach (string val in list2)
+            {
+                Console.WriteLine(val);
+            }
+
+            IEnumerable<string> list3 = fizzBuzz.GetFizzBuzz(Int32.MinValue, Int32.MaxValue);
+            foreach (string val in list3)
+            {
+                Console.WriteLine(val);
+            }
+        }
+    }
+}
---
+++ src/FizzLib/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzLib
+{
+    public class FizzBuzz
+    {
+        public IEnumerable<string> GetFizzBuzz(int min = 1, int max = 100)
+        {
+            IDictionary<int, string> defaultValues = new Dictionary<int, string>();
+            defaultValues.Add(3, "Fizz");
+            defaultValues.Add(5, "Buzz");
+            defaultValues.Add(15, "FizzBuzz");
+
+            return GetFizzBuzz(defaultValues, min, max);
+        }
+
+        public IEnumerable<string> GetFizzBuzz(IDictionary<int, string> displayValues, int min = 1, int max = 100)
+        {
+            var list = Enumerable.Range(1, 100)
+                .Select(n => {
+                    foreach(int i in displayValues.Keys.OrderByDescending(key => key))
+                    {
+                        if(n % i == 0)
+                        {
+                            return displayValues.FirstOrDefault(s => s.Key == i).Value;
+                        }
+                    }
+
+                    return n.ToString();
+                });
+
+            return list;
+        }
+    }
+}
