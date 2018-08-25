2988a847e23dbe72cf3618bde8f4dc91738057cf
Ryan Northfield <ryannorthfield@yahoo.com>
2018-08-25 17:36:07 -0500

    FizzBuzz

---
+++ src/FizzBuzz/FizzClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FizzLib;
+
+namespace FizzClient
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (string i in FizzBuzz.GetFizzBuzz(1, 100))
+            {
+                Console.Write(i);
+            }
+
+            foreach (string i in FizzBuzz.GetFizzBuzz(1, int.MaxValue))
+            {
+                Console.Write(i);
+            }
+
+            foreach (string i in FizzBuzz.GetFizzBuzz(int.MinValue, int.MaxValue))
+            {
+                Console.Write(i);
+            }
+        }
+    }
+}
---
+++ src/FizzBuzz/FizzLib/FizzBuzz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace FizzLib
+{
+    public static class FizzBuzz
+    {
+        public static IDictionary<string, List<int>> Multiples = new Dictionary<string, List<int>>()
+                                                                    {
+                                                                        {"Fizz", new List<int> {3}},
+                                                                        {"Buzz", new List<int> {5}},
+                                                                        {"FizzBuzz", new List<int> {3, 5}} 
+                                                                    };
+
+
+        public static IEnumerable<string> GetFizzBuzz(int min = 1, int max = 100)
+        {
+            var result = "";
+            bool isaMultiple = true;
+
+            for (int i = min; i < max; i++)
+            {
+                result = i.ToString();
+                foreach (var item in Multiples)
+                {
+                    foreach (var value in item.Value)
+                    {
+                        if (i % value != 0)
+                        {
+                            isaMultiple = false;
+                        }
+                    }
+
+                    if (isaMultiple)
+                    {
+                        result = item.Key;
+                    }
+                    isaMultiple = true;
+                }
+                yield return result;
+            }
+
+        }
+    }
+}
