8f4fa2f5b4ad0c33203b612f258f65f483ee68d5
Adam Letizia <adamjletizia@gmail.com>
2018-10-26 18:17:07 -0500

    write FizzBuzz result to console

--- src/FizzBuzz/FizzClient/FizzClient.cs
+++ src/FizzBuzz/FizzClient/FizzClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Autofac;
 using FizzClient.DependencyModule;
 using FizzLib.Services;
@@ -14,13 +15,13 @@ namespace FizzClient
             var fizzBuzz = container.Resolve<IFizzBuzz>();
 
             WriteRange(1, 100);
-            fizzBuzz.GetFizzBuzz();
+            WriteFizzBuzzToConsole(fizzBuzz.GetFizzBuzz());
 
             WriteRange(1, int.MaxValue);
-            fizzBuzz.GetFizzBuzz(max: int.MaxValue);
+            WriteFizzBuzzToConsole(fizzBuzz.GetFizzBuzz(max: int.MaxValue));
 
             WriteRange(int.MinValue, int.MaxValue);
-            fizzBuzz.GetFizzBuzz(int.MinValue, int.MaxValue);
+            WriteFizzBuzzToConsole(fizzBuzz.GetFizzBuzz(int.MinValue, int.MaxValue));
 
             Console.WriteLine("Press any key to quit.");
             Console.ReadKey();
@@ -29,6 +30,12 @@ namespace FizzClient
         private static void WriteRange(int min, int max)
         {
             Console.WriteLine($"min: {min}, max: {max}");
+        }
+
+        private static void WriteFizzBuzzToConsole(IEnumerable<string> results)
+        {
+            foreach (var result in results)
+                Console.WriteLine(result);
         }
     }
 }
