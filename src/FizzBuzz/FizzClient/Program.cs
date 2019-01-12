7cce67d542a7b29299549e899983752467c1a1e3
Brian Scheffler <scheffler.brian@gmail.com>
2019-01-12 15:39:29 -0600

    Finished Activity 2 - .NET Evaluation

---
+++ src/FizzBuzz/FizzClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FizzzLib;
+
+namespace FizzClient
+{
+    class Program
+    {
+        private const int ITERATOR_MAX = 10000;
+
+        static void Main(string[] args)
+        {
+            RunFizzBuzz(1, 100);
+
+            //RunFizzBuzz(1, Int32.MaxValue);
+
+            //RunFizzBuzz(Int32.MinValue, Int32.MaxValue);
+
+            Console.WriteLine(@"It's finally done!");
+            Console.ReadLine();
+        }
+
+        private static void RunFizzBuzz(int min, int max)
+        {
+            Console.WriteLine($"Running FizBuzz for {min} to {max}");
+            Console.WriteLine("---");
+
+            var i = min;
+            while(i < max)
+            {
+                var iteratorMin = i;
+                i += ITERATOR_MAX;
+                var iteratorMax = Math.Min(i, max);
+
+                var fizzBuzz = new FizzBuzz().GetFizzBuzz(iteratorMin, iteratorMax);
+                PrintValues(fizzBuzz);
+            }
+
+            Console.WriteLine("---");
+        }
+
+        private static void PrintValues(IEnumerable<string> values)
+        {
+            foreach (var value in values){
+                Console.WriteLine(value.ToString());
+            }
+        }
+    }
+}
---
+++ src/FizzBuzz/FizzzLib/FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzzLib
+{
+    public class FizzBuzz
+    {
+        public IEnumerable<string> GetFizzBuzz(int min =1, int max = 100)
+        {
+            var output = new List<string>();
+
+            for (int i = min; i <= max; i++)
+            {
+                output.Add(FizzBuzzLogic(i));
+            }
+
+            return output;
+        }
+
+        private string FizzBuzzLogic(int number)
+        {
+            var divisibleBy3 = IsDivisibleBy(number, 3);
+            var divisibleBy5 = IsDivisibleBy(number, 5);
+
+            if (divisibleBy3 && divisibleBy5)
+                return "Fizz Buzz";
+            if (divisibleBy3)
+                return "Fizz";
+            if (divisibleBy5)
+                return "Buzz";
+            return number.ToString();
+             
+        }
+
+        private bool IsDivisibleBy(int numerator, int denominator)
+        {
+            return numerator % denominator == 0;
+        }
+    }
+}
