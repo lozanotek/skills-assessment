f68c650251e1876793b53fa58536156dece748b7
manakd <67116077+manakd@users.noreply.github.com>
2020-06-18 16:22:38 -0500

    Add files via upload
    Updated FizzClient files

--- src/FizzBuzz/FizzClient/Program.cs
+++ src/FizzBuzz/FizzClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,49 @@ namespace FizzClient
     {
         static void Main(string[] args)
         {
+            int startval = 1;
+            int endval = 100;
+            int rangediff = 100;
+            try
+            {
+                if (args.Length > 0)
+                {
+                    startval = int.Parse(args[0]);
+                }
+                if (args.Length > 1)
+                {
+                    endval = int.Parse(args[1]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Format: FizzClient <minval> <maxval> (use valid int32 values)");
+                return;
+            }
+            int irangestart = 0;
+            int irangeend = 0;
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            irangestart = startval;
+            // use this so that if int32.MaxValue is selected, it won't overflow
+            irangeend = (endval - irangestart < rangediff) ? endval : irangestart + rangediff;
+            while (irangeend <= endval)
+            {
+                //Console.WriteLine("Start {0}, End {1}", irangestart, irangeend);
+                foreach (string sval in fizzBuzz.GetFizzBuzz(irangestart, irangeend))
+                {
+                    Console.WriteLine(sval);
+                }
+                // set next start and end values
+                if (irangeend < endval)
+                {
+                    irangestart = irangeend + 1;
+                    irangeend = (endval - irangestart < rangediff) ? endval : irangestart + rangediff;
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
