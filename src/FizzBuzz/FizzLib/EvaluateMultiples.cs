fc5037798dc9ab86c6ed9970d2c7670f7dc7807d
manakd <67116077+manakd@users.noreply.github.com>
2020-06-18 16:26:16 -0500

    Add files via upload
    Updated FizzLib files

---
+++ src/FizzBuzz/FizzLib/EvaluateMultiples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzLib
+{
+    static class EvaluateMultiples
+    {
+        static public string EvaluateInt(int ival)
+        {
+            string resultval = string.Empty;
+            if (ival % 3 == 0)
+            {
+                resultval += "Fizz";
+            }
+            if (ival % 5 == 0)
+            {
+                resultval += "Buzz";
+            }
+            if (resultval.Length == 0)
+            {
+                resultval = ival.ToString();
+            }
+            return (resultval);
+        }
+    }
+}
